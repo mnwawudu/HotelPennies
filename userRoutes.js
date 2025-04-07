bee9ac66c35eee5523026cce698756e529e171cf
mnwawudu <99718602+mnwawudu@users.noreply.github.com>
2025-04-07 16:30:42 +0100

    Update userRoutes.js

--- userRoutes.js
+++ userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/UserModel');
+const User = require('./UserModel');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
