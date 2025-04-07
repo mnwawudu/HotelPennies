33a60c7bfb34a3d079c4d3e09be1e4cf33e35958
mnwawudu <99718602+mnwawudu@users.noreply.github.com>
2025-04-07 23:12:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,12 +7,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true, useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'));
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
+// Route imports
 const userRoutes = require('./userRoutes');
+const shortletRoutes = require('./routes/shortletRoutes');
+const eventCenterRoutes = require('./routes/eventCenterRoutes');
+const bookingRoutes = require('./routes/bookingRoutes');
 
+// Route usage
 app.use('/api/user', userRoutes);
+app.use('/api/shortlets', shortletRoutes);
+app.use('/api/event-centers', eventCenterRoutes);
+app.use('/api/bookings', bookingRoutes);
 
+// Start server
 app.listen(5000, () => console.log('Server running on port 5000'));
