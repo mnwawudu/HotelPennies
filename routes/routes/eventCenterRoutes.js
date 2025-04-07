a4bb60d69fc61b1e32555f4d521585f7e2ba105a
mnwawudu <99718602+mnwawudu@users.noreply.github.com>
2025-04-07 23:45:46 +0100

    Create eventCenterRoutes.js

---
+++ routes/routes/eventCenterRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const eventCenters = [];
+
+router.post('/', (req, res) => {
+  const eventCenter = req.body;
+  eventCenters.push(eventCenter);
+  res.status(201).json(eventCenter);
+});
+
+router.get('/', (req, res) => {
+  res.json(eventCenters);
+});
+
+module.exports = router;
