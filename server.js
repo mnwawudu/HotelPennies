9b1d8681b5c322d8f62cae25fa8cd22b8403e50b
mnwawudu <99718602+mnwawudu@users.noreply.github.com>
2025-04-08 14:36:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,12 +9,11 @@ app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 }).then(() => console.log('MongoDB connected'))
-  .catch((err) => console.error('MongoDB connection error:', err));
+  .catch(err => console.error('Mongo connection error:', err));
 
-const userRoutes = require('./routes/userRoutes');
- // ✅ Make sure file is named exactly like this
+const userRoutes = require('./routes/userRoutes'); // must match exactly
 
 app.use('/api/user', userRoutes);
 
