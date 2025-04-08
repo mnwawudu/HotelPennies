3c6c6850601ab5c186da08e0aee3d94e182bd09b
mnwawudu <99718602+mnwawudu@users.noreply.github.com>
2025-04-08 03:14:45 +0100

    Update eventCenterRoutes.js

--- routes/routes/routes/eventCenterRoutes.js
+++ routes/routes/routes/eventCenterRoutes.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-// In-memory array to store event centers (temporary)
 const eventCenters = [];
 
-// Create a new event center
 router.post('/', (req, res) => {
   const eventCenter = req.body;
   eventCenters.push(eventCenter);
   res.status(201).json(eventCenter);
 });
 
-// Get all event centers
 router.get('/', (req, res) => {
   res.json(eventCenters);
 });
