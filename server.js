aa1e550a6ac0cb9e5565372b360a5c433b583840
mnwawudu <99718602+mnwawudu@users.noreply.github.com>
2025-04-08 22:53:17 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express();
 const userRoutes = require('./routes/userRoutes'); // ✅ Load userRoutes from the 'routes' folder
 
 app.use(express.json()); // Enable JSON body parsing
-
 app.use('/api/users', userRoutes); // ✅ Mount the user routes
 
 const PORT = process.env.PORT || 3000;
