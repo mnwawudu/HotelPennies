abf651a8160b44cbca36ab080e233f4e702d70e5
mnwawudu <99718602+mnwawudu@users.noreply.github.com>
2025-04-08 13:54:33 +0100

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,13 +2,13 @@ const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
   const token = req.header('Authorization')?.split(' ')[1];
-  if (!token) return res.status(401).json({ message: 'No token, authorization denied' });
+  if (!token) return res.status(401).json({ message: 'Access denied. No token provided.' });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
-  } catch (err) {
-    res.status(400).json({ message: 'Token is not valid' });
+  } catch (ex) {
+    res.status(400).json({ message: 'Invalid token.' });
   }
 };
