b91b51c5a5f69d667116bca5d719a80cccf09e5c
mnwawudu <99718602+mnwawudu@users.noreply.github.com>
2025-04-09 18:02:42 +0100

    Update advertRoutes.js

--- advertRoutes.js
+++ advertRoutes.js
@@ -1,25 +1,14 @@
-const express = require('express');
-const router = express.Router();
-const Advert = require('./advertModel');
+const mongoose = require('mongoose');
 
-// Create new advert
-router.post('/', async (req, res) => {
-  const { title, imageUrl, link } = req.body;
-  const newAdvert = new Advert({ title, imageUrl, link });
-  await newAdvert.save();
-  res.json({ message: 'Advert created', advert: newAdvert });
+const advertSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  imageUrl: String,
+  link: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-// Get all adverts
-router.get('/', async (req, res) => {
-  const adverts = await Advert.find().sort({ createdAt: -1 });
-  res.json(adverts);
-});
-
-// Delete advert
-router.delete('/:id', async (req, res) => {
-  await Advert.findByIdAndDelete(req.params.id);
-  res.json({ message: 'Advert deleted' });
-});
-
-module.exports = router;
+module.exports = mongoose.model('Advert', advertSchema);
