4062cc7e7e39ed048f34388a95c4a024efb4e088
mnwawudu <99718602+mnwawudu@users.noreply.github.com>
2025-04-09 10:04:13 +0100

    Delete routes directory

--- routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// ✅ Simple test route to confirm everything works
-router.get('/', (req, res) => {
-  res.send('User route is working!');
-});
-
-module.exports = router;
