49116a06c83d5cfbfd84af53754bf9080dd61b7f
mnwawudu <99718602+mnwawudu@users.noreply.github.com>
2025-04-11 19:11:12 +0100

    Update UserModel.js

--- UserModel.js
+++ UserModel.js
@@ -5,37 +5,37 @@ const userSchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
-      trim: true
+      trim: true,
     },
     email: {
       type: String,
       required: true,
       unique: true,
-      lowercase: true
+      lowercase: true,
     },
     password: {
       type: String,
-      required: true
+      required: true,
     },
     affiliateCode: {
       type: String,
-      unique: true
+      unique: true,
     },
     referredBy: {
-      type: String, // Can later be changed to ObjectId if referencing another user
-      default: null
+      type: String, // Can be changed to ObjectId if linking to another user
+      default: null,
     },
     commissions: {
       type: Number,
-      default: 0
+      default: 0,
     },
     payouts: {
       type: Number,
-      default: 0
-    }
+      default: 0,
+    },
   },
   {
-    timestamps: true // Automatically adds createdAt and updatedAt
+    timestamps: true, // Adds createdAt and updatedAt automatically
   }
 );
 
