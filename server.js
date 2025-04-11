3559d3a93bfbf7b98e5479e0b455b92ec9873491
mnwawudu <99718602+mnwawudu@users.noreply.github.com>
2025-04-11 23:26:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const dotenv = require('dotenv');
 // Route files
 const userRoutes = require('./userRoutes');
 const dashboardRoutes = require('./dashboardRoute');
-const payoutRoutes = require('./payoutRoutes'); // ✅ ADD THIS LINE
+const payoutRoutes = require('./payoutRoutes'); // ✅ Add this line
 
 dotenv.config();
 
@@ -29,7 +29,7 @@ mongoose.connect(process.env.MONGO_URI, {
 // Route registrations
 app.use('/api/users', userRoutes);
 app.use('/api', dashboardRoutes);
-app.use('/api/payouts', payoutRoutes); // ✅ ADD THIS LINE
+app.use('/api/payouts', payoutRoutes); // ✅ Add this line
 
 app.get('/', (req, res) => {
   res.send('HotelPennies API is running...');
