530dd5a841b4a39a413e4836d53a6aec26ef1bd7
mnwawudu <99718602+mnwawudu@users.noreply.github.com>
2025-04-12 00:55:14 +0100

    Create advertModel.js

---
+++ advertModel.js
@@ -0,0 +1,15 @@
+// advertModel.js
+const mongoose = require('mongoose');
+
+const advertSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  imageUrl: String,
+  link: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Advert', advertSchema);
