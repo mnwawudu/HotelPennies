e6dc148a84fe9cc55ed5acbde267427688f9a4d8
mnwawudu <99718602+mnwawudu@users.noreply.github.com>
2025-04-12 21:09:52 +0100

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import AdvertsSection from './AdvertsSection'; // No folder, just root
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to HotelPennies</h1>
+      <AdvertsSection />
+    </div>
+  );
+}
+
+export default App;
